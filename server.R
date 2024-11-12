5edc3e883e9534cfe92114eccbcf6841a4222b09
Liam Surry <135068729+liamthepineapple@users.noreply.github.com>
2024-11-12 13:52:16 -0500

    Updated server for "about" tab

--- server.R
+++ server.R
@@ -2,6 +2,37 @@
 #Initialized 2024-11-12 @McMaster University
 #Department of Chemistry and Chemical Biology 
 
+
+#Initialize server
 server <- function(input, output, session) {
   
+  
+  #About tab information
+  output$readmeContent <- renderUI({
+    content <- readLines("README.md") %>%
+      paste(collapse = "\n")
+    HTML(markdown::markdownToHTML(text = content))
+  })
+  
+  output$licenseContent <- renderUI({
+    content <- readLines("LICENSE.md") %>%
+      paste(collapse = "\n")
+    HTML(markdown::markdownToHTML(text = content))
+  })
+  
+  output$disclaimerContent <- renderUI({
+    content <- readLines("DISCLAIMER.md") %>%
+      paste(collapse = "\n")
+    HTML(markdown::markdownToHTML(text = content))
+  })
+  
+  output$updatesContent <- renderUI({
+    content <- readLines("UPDATES.md") %>%
+      paste(collapse = "\n")
+    HTML(markdown::markdownToHTML(text = content))
+  })
+  
+  
 }
+  
+

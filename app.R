df8a4a6304637eef56eeed2093662a682c3d38fb
Liam Surry <135068729+liamthepineapple@users.noreply.github.com>
2024-11-13 11:27:34 -0500

    Updated app.R script to call directly from ui.R and server.R script

--- app.R
+++ app.R
@@ -2,6 +2,8 @@
 #Initialized 2024-11-12 @McMaster University
 #Department of Chemistry and Chemical Biology 
 
-
 # Run the application 
+source("ui.R")
+source("server.R")
+
 shinyApp(ui = ui, server = server)

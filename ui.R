e2301b632240c033488e28ece3152118a1d51a2c
Liam Surry <135068729+liamthepineapple@users.noreply.github.com>
2025-03-19 12:37:15 -0400

    Added button for deleting all middle peaks

--- ui.R
+++ ui.R
@@ -342,6 +342,8 @@ ui <- dashboardPage(
                        actionButton("adjust_all_baselines", "Adjust All Baselines"),
                        #Action button for regenerating plots
                        actionButton("regenerateplots", "Regenerate Changed Plots"),
+                       #Action button for deleting all but the first and last peaks.
+                       actionButton("deletemiddlepeaks","Delete Middle Peaks (Keeps first and last)"),
                        
                        #Data table for peak positions
                        tags$h4(style = "text-decoration: underline;", "Peak Position Information"),
